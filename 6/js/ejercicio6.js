e89890bee3615fecab9322e8e435483ed08838f3
Santiago Puertas <santidoors13@gmail.com>
2024-03-21 05:25:56 -0300

    Sexto ejercicio terminado

---
+++ 6/js/ejercicio6.js
@@ -0,0 +1,11 @@
+let num1;
+let num2;
+
+num1 = parseInt(prompt("Ingresa un numero:"));
+num2 = parseInt(prompt("Ingresa otro numero:"));
+
+if (num1 > num2) {
+  alert("El mayor es " + num1);
+} else {
+  alert("El mayor es " + num2);
+}
