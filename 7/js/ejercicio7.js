5d81fdc7edeae92a6f1f062b4ba49533823165fa
Santiago Puertas <santidoors13@gmail.com>
2024-03-21 05:30:34 -0300

    Septimo ejercicio terminado

---
+++ 7/js/ejercicio7.js
@@ -0,0 +1,15 @@
+let num1;
+let num2;
+let num3;
+
+num1 = parseInt(prompt("Ingresa el primer numero:"));
+num2 = parseInt(prompt("Ingresa el segundo numero:"));
+num3 = parseInt(prompt("Ingresa el tercer numero:"));
+
+if (num1 > num2 && num1 > num3) {
+  alert("El mayor es " + num1);
+} else if (num2 > num1 && num2 > num3) {
+  alert("El mayor es " + num2);
+} else {
+  alert("El mayor es " + num3);
+}
