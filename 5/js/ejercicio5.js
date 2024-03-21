8dad65b2b19e1a4780fe10de8a6b9c7d9349b81c
Santiago Puertas <santidoors13@gmail.com>
2024-03-21 05:23:28 -0300

    Quinto ejercicio terminado

---
+++ 5/js/ejercicio5.js
@@ -0,0 +1,6 @@
+let num1;
+let num2;
+num1 = parseInt(prompt("Ingresa un numero:"));
+num2 = parseInt(prompt("Ingresa otro numero:"));
+num3 = num1 + num2;
+document.write("El resultado de la suma de los numeros es " + num3);
