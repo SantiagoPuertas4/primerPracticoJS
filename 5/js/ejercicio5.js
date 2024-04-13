7da3354b0252c3d992cc896a02f1bfa043980ed4
Santiago Puertas <santidoors13@gmail.com>
2024-04-13 04:29:16 -0300

    Agregada validacion al ejercicio 5

--- 5/js/ejercicio5.js
+++ 5/js/ejercicio5.js
@@ -2,5 +2,12 @@ let num1;
 let num2;
 num1 = parseInt(prompt("Ingresa un numero:"));
 num2 = parseInt(prompt("Ingresa otro numero:"));
-num3 = num1 + num2;
-document.write("El resultado de la suma de los numeros es " + num3);
+
+if (num1 === null || num2 === null) {
+  alert("No ingreso uno o los dos numeros");
+} else if (isNaN(num1) || isNaN(num2)) {
+  alert("Uno o los dos numeros ingresados no son validos");
+} else {
+  num3 = num1 + num2;
+  document.write("El resultado de la suma de los numeros es " + num3);
+}
