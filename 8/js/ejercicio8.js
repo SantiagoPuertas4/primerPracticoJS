10f912f8376a9020a7afb407175e98f5bd64c178
Santiago Puertas <santidoors13@gmail.com>
2024-04-13 04:49:16 -0300

    Agregada validacion al ejercicio 8

--- 8/js/ejercicio8.js
+++ 8/js/ejercicio8.js
@@ -1,9 +1,17 @@
 let num1;
 
-num1 = parseInt(prompt("Ingresa un numero :"));
+num1 = prompt("Ingresa un numero :");
 
-if (num1 % 2 === 0) {
-  alert("Es divisible por 2");
+if (num1 === null) {
+  alert("No se ingreso nada");
+} else if (isNaN(num1)) {
+  alert("El numero ingresado no es valido");
 } else {
-  alert("No es divisible por 2");
+  num1 = parseInt(num1);
+
+  if (num1 % 2 === 0) {
+    alert("Es divisible por 2");
+  } else {
+    alert("No es divisible por 2");
+  }
 }
