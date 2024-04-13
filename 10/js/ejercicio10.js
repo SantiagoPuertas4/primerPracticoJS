d34e592a3e9da01ee0dc4f83e1acbac802ac900f
Santiago Puertas <santidoors13@gmail.com>
2024-04-13 04:51:47 -0300

    Agregada validacion al ejercicio 10

--- 10/js/ejercicio10.js
+++ 10/js/ejercicio10.js
@@ -1,15 +1,23 @@
 let num1;
 
-num1 = parseInt(prompt("Ingresa un numero :"));
+num1 = prompt("Ingresa un numero :");
 
-if (num1 % 2 === 0) {
-  alert("Es divisible por 2");
-} else if (num1 % 3 === 0) {
-  alert("Es divisible por 3");
-} else if (num1 % 5 === 0) {
-  alert("Es divisible por 5");
-} else if (num1 % 7 === 0) {
-  alert("Es divisible por 7");
+if (num1 === null) {
+  alert("No se ingreso nada");
+} else if (isNaN(num1)) {
+  alert("El numero ingresado no es valido");
 } else {
-  alert("No es divisible por ningun numero preestablecido");
+  num1 = parseInt(num1);
+
+  if (num1 % 2 === 0) {
+    alert("Es divisible por 2");
+  } else if (num1 % 3 === 0) {
+    alert("Es divisible por 3");
+  } else if (num1 % 5 === 0) {
+    alert("Es divisible por 5");
+  } else if (num1 % 7 === 0) {
+    alert("Es divisible por 7");
+  } else {
+    alert("No es divisible por ningun numero preestablecido");
+  }
 }
