751d0002684743904e025c47e8e1f8e25a879463
Santiago Puertas <santidoors13@gmail.com>
2024-04-13 00:52:18 -0300

    Arreglado bug de ejercicio 7

--- 7/js/ejercicio7.js
+++ 7/js/ejercicio7.js
@@ -8,7 +8,7 @@ num3 = parseInt(prompt("Ingresa el tercer numero:"));
 
 if (num1 > num2 && num1 > num3) {
   alert("El mayor es " + num1);
-} else if (num2 > num1 && num2 > num3) {
+} else if ((num2 > num1 && num2 > num3) || num1 === num2) {
   alert("El mayor es " + num2);
 } else {
   alert("El mayor es " + num3);
